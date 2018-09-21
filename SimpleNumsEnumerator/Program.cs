0af1383c79f30e7d8b3222ac518e53f498bb6c80
newLana <newLana@users.noreply.github.com>
2018-09-21 15:27:47 +0300

    Add project files.

---
+++ SimpleNumsEnumerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SimpleNumsEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int[] array;
+                #region CustomArray
+                Console.WriteLine("Enter elements of integer array. Separates each element by whitespace.");
+                var strElements = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                array = new int[strElements.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (!int.TryParse(strElements[i], out array[i]))
+                        throw new ArgumentException("Cannot convert string array to int array. There is unconvertible element.");
+                }
+                #endregion
+
+                #region HardCodedArray
+                //array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 27, 29, 31, 33 };
+                #endregion
+
+                var simpleNums = new SimpleNums(array);
+                foreach (var item in simpleNums)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (ArgumentException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleNumsEnumerator/SimpleNums.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace SimpleNumsEnumerator
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    class SimpleNums
+    {
+        private int[] innerArray { get; set; }
+
+        public SimpleNums(int[] array)
+        {
+            if (array.Any(a => a <= 0))
+                throw new ArgumentException("Simple numbers must be a natural. It means that array must contains only greater than 0 numbers.");
+            innerArray = new int[array.Length];
+            Array.Copy(array, innerArray, array.Length);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new SimpleNumsEnumerator(innerArray);
+        }
+    }
+
+    class SimpleNumsEnumerator : IEnumerator
+    {
+        private int[] innerArray { get; set; }
+        int position = -1;
+
+        public SimpleNumsEnumerator(int[] nums)
+        {
+            innerArray = nums.Where(n => IsSimple(n)).ToArray();
+        }       
+
+        object IEnumerator.Current => innerArray[position];
+
+        public bool MoveNext() => ++position < innerArray.Length;
+
+        public void Reset() => position = -1;
+
+        private static bool IsSimple(int number)
+        {
+            int start = 2;
+            while (start < number)
+            {
+                if (number % start++ == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
